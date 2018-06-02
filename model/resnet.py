c4837de6c892ca3c1c604e6ee3646f257b06d884
numpee <dongwan123@gmail.com>
2018-06-02 16:41:14 +0900

    Update resnet

--- model/resnet.py
+++ model/resnet.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as f
 import numpy as np
 
+def make_model(args):
+    return resnet34(args)
+
 class BasicConv(nn.Module):
 
     def __init__(self, in_channels, out_channels, **kwargs):
@@ -22,7 +25,9 @@ class BasicConv(nn.Module):
         x = self.relu(x)
         return x
 
+# Basic Resnet Block
 class BasicBlock(nn.Module):
+    expansion = 1
     
     def __init__(self, in_channels, out_channels, stride = 1, downsample=None, **kwargs):
         super(BasicBlock, self).__init__()
@@ -33,6 +38,77 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
         self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+    
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = BasicConv(3, 64, kernel_size = 7, stride = 2, padding = 3, bias = False)
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        self.layer1 = self.make_layer(block, 64, layers[0])
+        self.layer2 = self.make_layer(block, 128, layers[1], stride = 2)
+        self.layer3 = self.make_layer(block, 256, layers[2], stride = 2)
+        self.layer4 = self.make_layer(block, 512, layers[3], stride = 2)
+        self.conv2 = BasicConv(512, 128, kernel_size = 1)
+        
+        self.fc1 = nn.Linear(5760, 82)
+
+    
+    def make_layer(self, block, planes, blocks, stride = 1):
+        downsample = None
+        if stride != 1 or self.inplanes !=planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size = 1, stride = stride, bias = False),
+                nn.BatchNorm2d(planes),
+            )
+        
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+        
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.maxpool(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.conv2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+
+        return out
+
+
+def resnet34(args):
+    model = ResNet(BasicBlock, [3, 4, 6, 3])
+
+    return model
 
 
         
