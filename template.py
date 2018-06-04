dc5826b37f6b04aa44c5c05a9dfe70c5115f75a7
esw0116 <esw0116@gmail.com>
2018-06-04 20:54:03 +0900

    update template

--- template.py
+++ template.py
@@ -3,9 +3,9 @@ def set_template(args):
     if args.template.find('SY') >= 0:
         args.work_type = 'Character'
         args.model = 'baseline'
-        args.batch_size = 32
-        args.num_batches = 16000
-        args.decay_step = 5000
+        args.batch_size = 128
+        args.num_epochs = 10
+        args.decay_step = 1
         args.learning_rate = 2e-3
         args.gamma = 0.5
 
@@ -22,7 +22,7 @@ def set_template(args):
 	    args.work_type='Character'
 	    args.model = 'resnet'
 	    args.batch_size = 128
-	    args.epochs = 10
+	    args.num_epochs = 10
 	    args.learning_rate = 2e-3
 	    args.decay_step = 1
 	    args.gamma = 0.5 
