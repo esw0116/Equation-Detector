efd5a2cb750bd3169047db57a4bdcd386f516269
numpee <dongwan123@gmail.com>
2018-06-05 02:15:13 +0900

    update CRNN

--- model/CRNN.py
+++ model/CRNN.py
@@ -1,13 +1,66 @@
 import torch.nn as nn
 
-from model import baseline
-from model import common
+from model import model
 
 
 def make_model(args):
     CRNN(args)
 
+# input model name for model
+class EncoderCNN(nn.Module):
+    def __init__(self, model, ckp):
+        super(EncoderCNN, self).__init__()
+        my_model = model.make_model()
+        my_model.reset()
+        self.ckp = ckp
+        
+        my_model.load_state_dict(torch.load(os.path.join(ckp.log_dir, 'model', 
+                'model_best.pt'), **kwargs), strict=False)
+        
+        # delete Fully Connected layer
+        modules = list(my_model.childre())[:-1]
+        self.my_model = nn.Sequential(*modules)
+    
+    def forward(self, x):
+        with torch.no_grad():       ## remove if fine tuning
+            features = self.my_model(x)
+        #flatten for RNN Input
+        features = features.reshape(features.size(0), -1)
+        return features
 
+class BidirectionalLSTM(nn.Module):
+
+    def __init__(self, args, embed_size, hidden_size, lexicon_size, num_layers, max_seq_length=96):
+        super(CRNN, self).__init__()
+        self.embed = nn.Embedding(lexicon_size, embed_size)
+        # num layers can be 1 or 2
+        self.bilstm = nn.LSTM(embed_size, hidden_size, num_layers, bidirectional=True, batch_first = True)
+        self.linear = nn.Linear(hidden_size, lexicon_size)
+        self.max_seq_length = max_seq_length
+
+    def forward(self, features, labels, lengths):
+        embeddings = self.embed(labels)
+        # batch at dimension 0
+        embeddings = torch.cat((features.unsqueeze(1), embeddings),1)
+        packed = pack_padded_sequence(embeddings, lengths, batch_first = True)
+        hiddens, _ = self.lstm(packed)
+        outputs = self.linear(hiddens[0])
+        return outputs
+
+    def sample(self, features, states = None):
+        sampled_idx = []
+        inputs = features.unsqueeze(1)
+        for i in range(self.max_seq_length):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            _, predicted = outputs.max(1)
+            sampled_idx.append(predicted)
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        sampled_idx = torch.stack(sampled_idx, 1)
+        return sampled_idx
+
+'''
 class BidirectionalLSTM(nn.Module):
 
     def __init__(self, nIn, nHidden, nOut):
@@ -27,19 +80,4 @@ class BidirectionalLSTM(nn.Module):
 
         return output
 
-
-class CRNN(baseline.baseline, BidirectionalLSTM):
-
-    def __init__(self, args, conv=common.default_conv):
-        super(CRNN, self).__init__(args, conv)
-        self.cnn = nn.Sequential(*list(baseline.baseline.features.children())[0:-2])
-        self.lstm = BidirectionalLSTM.features.children()
-
-    def forward(self, x):
-        cnn = self.cnn(x)
-        B, _, _, _ = cnn.size()
-        cnn = cnn.view(96, B, -1)
-        # LSTM input shape (Sequence length, Batch size, input vector size)
-        rnn = self.lstm(cnn)
-
-        return rnn
+'''
